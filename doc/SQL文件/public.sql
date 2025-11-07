c34771903fa7c77cea7bfb3e08056a07e3ec3978
一陌千尘 <18780166539@189.cn>
2025-11-07 09:55:51 +0800

    上传SQL文件

---
+++ doc/SQL文件/public.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost-PostgresSQL-本地
+ Source Server Type    : PostgreSQL
+ Source Server Version : 170006 (170006)
+ Source Host           : localhost:5432
+ Source Catalog        : origin
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 170006 (170006)
+ File Encoding         : 65001
+
+ Date: 07/11/2025 09:50:50
+*/
+
