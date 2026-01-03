f92c5cc19adaa2b4b2d4a61b34c6b0060a709a97
一陌千尘 <18780166539@189.cn>
2026-01-04 02:03:42 +0800

    Maven依赖版本更新

--- origin-module-manage/src/test/java/com/cosmos/origin/manage/OriginModuleManageApplicationTests.java
+++ origin-module-manage/src/test/java/com/cosmos/origin/manage/OriginModuleManageApplicationTests.java
@@ -3,8 +3,6 @@ package com.cosmos.origin.manage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.UUID;
-
 @SpringBootTest
 class OriginModuleManageApplicationTests {
 
